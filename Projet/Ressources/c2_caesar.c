d5b60c22fb8dfc4390e6abe8d2ee0b65b692006e
Espaew <gallic.maxime@gmail.com>
2026-01-14 10:44:20 +0100

    Update c2_caesar.c

--- Projet/Ressources/c2_caesar.c
+++ Projet/Ressources/c2_caesar.c
@@ -4,22 +4,17 @@
 #include <arpa/inet.h>
 
 char caesar(char c, int shift) {
-    if (c >= 'a' && c <= 'z')
-        return 'a' + (c - 'a' + shift + 26) % 26;
-    if (c >= 'A' && c <= 'Z') // Ajout pour les majuscules
-        return 'A' + (c - 'A' + shift + 26) % 26;
+    if (c >= 'a' && c <= 'z') return 'a' + (c - 'a' + shift + 26) % 26;
+    if (c >= 'A' && c <= 'Z') return 'A' + (c - 'A' + shift + 26) % 26;
     return c;
 }
 
 void caesar_cipher(char *s, int shift) {
-    for (int i = 0; s[i]; i++)
-        s[i] = caesar(s[i], shift);
+    for (int i = 0; s[i]; i++) s[i] = caesar(s[i], shift);
 }
 
 int main() {
     char buffer[128];
-
-    // --- PHASE SERVEUR (Reçoit de C1) ---
     int server = socket(AF_INET, SOCK_STREAM, 0);
     int opt = 1;
     setsockopt(server, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt));
@@ -30,39 +25,42 @@ int main() {
     local.sin_addr.s_addr = INADDR_ANY;
 
     bind(server, (struct sockaddr*)&local, sizeof(local));
-    listen(server, 1);
+    listen(server, 5);
 
     printf("[C2] En attente de C1 sur le port 5000...\n");
-    int client = accept(server, NULL, NULL);
-    int len = recv(client, buffer, sizeof(buffer)-1, 0);
-    if (len > 0) {
-        buffer[len] = 0;
-        printf("[C2] Reçu de C1 : %s\n", buffer);
-        
-        caesar_cipher(buffer, 5);
-        printf("[C2] Après César (+5) : %s\n", buffer);
+    fflush(stdout);
 
-        // --- PHASE CLIENT (Envoie à C3 sur VM2) ---
-        int sock = socket(AF_INET, SOCK_STREAM, 0);
-        struct sockaddr_in c3 = {0};
-        c3.sin_family = AF_INET;
-        c3.sin_port = htons(5001);
-        
-        // ATTENTION : Ici, mettre l'IP de la VM2 !
-        c3.sin_addr.s_addr = inet_addr("192.168.200.20"); 
+    while (1) { // Début de la boucle infinie
+        int client = accept(server, NULL, NULL);
+        int len = recv(client, buffer, sizeof(buffer)-1, 0);
+        if (len > 0) {
+            buffer[len] = 0;
+            printf("[C2] Reçu de C1 : %s\n", buffer);
+            fflush(stdout);
 
-        printf("[C2] Connexion à C3 (VM2)... \n");
-        while(connect(sock, (struct sockaddr*)&c3, sizeof(c3)) < 0) {
-            perror("Erreur connexion C3");
-            sleep(2);
+            caesar_cipher(buffer, 5);
+            printf("[C2] Après César (+5) : %s\n", buffer);
+            fflush(stdout);
+
+            int sock = socket(AF_INET, SOCK_STREAM, 0);
+            struct sockaddr_in c3 = {0};
+            c3.sin_family = AF_INET;
+            c3.sin_port = htons(5001);
+            c3.sin_addr.s_addr = inet_addr("192.168.200.20"); 
+
+            printf("[C2] Connexion à C3 (VM2)... \n");
+            fflush(stdout);
+
+            if (connect(sock, (struct sockaddr*)&c3, sizeof(c3)) >= 0) {
+                send(sock, buffer, strlen(buffer), 0);
+                printf("[C2] Données envoyées à C3.\n");
+                fflush(stdout);
+		close(sock);
+            } else {
+                perror("Erreur connexion C3");
+            }
         }
-        
-        send(sock, buffer, strlen(buffer), 0);
-        printf("[C2] Données envoyées à C3.\n");
-        close(sock);
+        close(client); // On ferme le client mais PAS le serveur
     }
-
-    close(client);
-    close(server);
     return 0;
 }
